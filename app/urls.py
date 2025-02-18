861ca2b0efb03cc31fc84bd33f9b10382d36eda7
diyorbek <dturgunboyev635@gmail.com>
2025-02-18 10:45:17 +0500

    update

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,15 @@
 from django import urls
 from django.urls import path
-from app.views import IndexPageTemplateView,HomePageTemplateView,LoginPage,TextPage,Image,Code,Settings,Project,Template
+from app.views import IndexPageTemplateView,HomePageTemplateView,RegisterView,TextPage,Image,Code,Settings,Project,Template,Support
 urlpatterns = [
     path('index/',IndexPageTemplateView.as_view(),name='index'),
     path('home/',HomePageTemplateView.as_view(),name='home'),
-    path('login/',LoginPage.as_view(),name='login'),
+    path('login/',RegisterView.as_view(),name='login'),
     path('text/',TextPage.as_view(),name='text'),
     path('image/',Image.as_view(),name='image'),
     path('code/',Code.as_view(),name='code'),
     path('setting/',Settings.as_view(),name='setting'),
     path('project/',Project.as_view(),name='project'),
-    path('template/',Template.as_view(),name='template')
+    path('template/',Template.as_view(),name='template'),
+    path('support/',Support.as_view(),name='support'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
-
-
+from django.views import View
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from app.forms import RegisterForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 class IndexPageTemplateView(TemplateView):
     template_name = 'index.html'
-
+@method_decorator(login_required, name='dispatch')
 class HomePageTemplateView(TemplateView):
     template_name = 'dashboard.html'
 
-class LoginPage(TemplateView):
-    template_name = 'login.html'
 
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        return render(request, 'login.html', {'form': form})
+
+    def post(self, request):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])  # Hash the password
+            user.save()
+            login(request, user)  # Auto-login after registration
+            return redirect(reverse_lazy('home'))  # Redirect to dashboard
+        return render(request, 'login.html', {'form': form})
 class TextPage(TemplateView):
     template_name = 'text.html'
 
@@ -29,3 +46,6 @@ class Project(TemplateView):
 
 class Template(TemplateView):
     template_name = 'template.html'
+
+class Support(TemplateView):
+    template_name = 'support.html'
